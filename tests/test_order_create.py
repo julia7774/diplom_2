960f68cfdef31d888b43d99cf7c1ec3c8b5a92de
Julia Bryukhanova <jobryukhanova@yandex.ru>
2026-01-18 09:55:17 +0500

    Правки после ревью

--- tests/test_order_create.py
+++ tests/test_order_create.py
@@ -22,6 +22,23 @@ class TestOrderCreate:
         assert "success" in json_
         assert json_["success"] == True
 
+    @allure.title("Проверка создания заказа без авторизации")
+    def test_unauthorized(self):
+        ingredients = StellarBurgersClient.ingredients_list()
+
+        with allure.step("Отправить POST запрос на создание заказа"):
+            response = StellarBurgersClient.order_create({"ingredients": ingredients}, {})
+
+        assert response.status_code == 200
+        json_ = response.json()
+        assert "name" in json_
+        assert json_["name"] != ""
+        assert "order" in json_
+        assert "number" in json_["order"]
+        assert json_["order"]["number"] > 0
+        assert "success" in json_
+        assert json_["success"] == True
+
     @allure.title("Проверка создания заказа если в запросе передан невалидный хэш ингредиента")
     def test_invalid_ingredient(self, user):
         _, _, _, token = user
